a7fe2aec094d20833281ef4c871a669a187d68c5
joyoo9442 <joyoo9442@gmail.com>
2024-03-20 15:36:50 -0400

    Fixed the apple dataset good bad issue on lr

--- lr_apple.py
+++ lr_apple.py
@@ -13,9 +13,29 @@ data = df.to_numpy()
 X = data[:, 0:-1]
 y = data[:, -1]
 
+# Load test data from kaggle website
+df_test = pd.read_csv('apple_quality.csv')
+df_test.Quality = df_test.Quality.map({'good': 1, 'bad': 0})
+data_test = df_test.to_numpy()
+X_apple = data_test[:, 1:-1]
+y_apple = data_test[:, -1]
+
+# for i in range(len(y_apple)):
+#     if y_apple[i] == 'good':
+#         y_apple[i] = 1
+#     else:
+#         y_apple[i] = 0
+
 # Standardize the data
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
+
+# Standardize the test data
+scaler = StandardScaler()
+X_apple = scaler.fit_transform(X_apple)
+
+# Split the test data into training and testing set
+X_train, X_test, y_train, y_test = train_test_split(X_apple, y_apple, test_size=0.2, random_state=42)
 
 # Dont need to split the data into training and testing set
 
@@ -27,3 +47,7 @@ model.fit(X, y)
 
 # Output the model
 print(model.coef_)
+
+# Test the model
+y_pred = model.predict(X_test)
+print(accuracy_score(y_test, y_pred))
--- svm_apple.py
+++ svm_apple.py
@@ -13,9 +13,25 @@ data = df.to_numpy()
 X = data[:, 0:-1]
 y = data[:, -1]
 
+# Load test data from kaggle website
+df_test = pd.read_csv('apple_quality.csv')
+data_test = df_test.to_numpy()
+X_test = data_test[:, 1:-1]
+y_test = data_test[:, -1]
+
+for i in range(len(y_test)):
+    if y_test[i] == 'good':
+        y_test[i] = 1
+    else:
+        y_test[i] = 0
+
 # Standardize the data
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
+
+# Standardize the test data
+scaler = StandardScaler()
+X_test = scaler.fit_transform(X_test)
 
 # Dont need to split the data into training and testing set
 
@@ -25,5 +41,9 @@ model = SVC()
 # Train the model
 model.fit(X, y)
 
+# Test the model
+y_pred = model.predict(X_test)
+
 # Output the model
 print(model.support_vectors_)
+print(accuracy_score(y_test, y_pred))
